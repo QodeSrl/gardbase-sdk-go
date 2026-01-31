43ae3dcbdbdef4cd91b35cf3754ebe4fcc11368e
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2026-01-31 23:50:09 +0100

    Add Cache implementation for managing key-value pairs with persistent storage

---
+++ internal/cache.go
@@ -0,0 +1,58 @@
+package internal
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+type Cache struct {
+	mu   sync.RWMutex
+	data map[string]any
+	path string
+}
+
+func NewCache(cacheDir string) (*Cache, error) {
+	c := &Cache{
+		data: make(map[string]any),
+		path: filepath.Join(cacheDir, "cache.json"),
+	}
+
+	if _, err := os.Stat(cacheDir); err == nil {
+		bytes, err := os.ReadFile(c.path)
+		if err != nil {
+			return nil, err
+		}
+		if len(bytes) > 0 {
+			if err := json.Unmarshal(bytes, &c.data); err != nil {
+				return nil, err
+			}
+		}
+	}
+	return c, nil
+}
+
+func (c *Cache) Get(key string) (any, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	val, ok := c.data[key]
+	return val, ok
+}
+
+func (c *Cache) Set(key string, value any) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.data[key] = value
+	return c.save()
+}
+
+func (c *Cache) save() error {
+	bytes, err := json.MarshalIndent(c.data, "", "   ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(c.path, bytes, 0644)
+}
